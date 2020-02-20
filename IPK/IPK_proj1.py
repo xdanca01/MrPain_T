0d31c09d8bdb961d6246d5a09f907848751423af
xdanca01 <pdancak@redhat.com>
2020-02-20 16:50:47 +0100

    now can connect and receive msg

--- IPK/IPK_proj1.py
+++ IPK/IPK_proj1.py
@@ -3,6 +3,8 @@
 import socket
 import sys
 import string
+import os
+import re
 
 argc = len(sys.argv)
 if argc != 2:
@@ -17,8 +19,28 @@ if not sys.argv[1].isdigit():
 #PORT
 ServerPort = int(sys.argv[1])
 #localhost
+SIZE = 1024
 Server = '127.0.0.1'
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.bind((Server, ServerPort))
 clientSocket.listen()
-conn, addr = clientSocket.accept()
+while True:
+    conn, addr = clientSocket.accept()
+    commID = os.fork()
+    #child leave
+    if commID == 0:
+        break
+#create new connection with client addr on socket conn
+print("connection with client",addr,"ESTABLISHED")
+BUFFER = conn.recv(SIZE)
+BUFFER = BUFFER.decode('utf-8')
+if not BUFFER:
+    print("nothing received")
+REQUEST = re.search("GET", BUFFER)
+print(REQUEST)
+
+
+
+print("konec")
+conn.close()
+exit(0)
