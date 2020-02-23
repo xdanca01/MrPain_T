64181a9b1c838a424a752426afd39298b4971798
xdanca01 <petrik2008@email.cz>
2020-02-23 15:08:32 +0100

    push for ubuntu

--- IPK/IPK_proj1.py
+++ IPK/IPK_proj1.py
@@ -30,46 +30,79 @@ while True:
     #create new connection with client addr on socket conn
     BUFFER = conn.recv(SIZE).decode()
     if not BUFFER:
-        print("unknown method")
-        conn.send(b'HTTP/1.1 405 Method Not Allowed\n')
+        print("got nothing")
+        conn.sendall(b'HTTP/1.1 500 Internal Server Error\n')
     else:
         REQUEST = re.search("GET", BUFFER)
+        #GET
         if REQUEST:
-            print(BUFFER)
-            GET = re.findall('=.*&', BUFFER)
-            GET = GET[0][1:-1]
-            REQUEST = re.findall('=A HTTP/1.1', BUFFER)
-            #A hostname -> IP
-            if REQUEST:
-                ADDR = socket.gethostbyname(GET)
-                BUFFER = 'HTTP/1.1 200 OK\n' + GET + ":A=" + ADDR + "\n"
-                conn.sendall(BUFFER.encode())
-            #PTR IP -> hostname
+            TEST = re.search("/resolve", BUFFER)
+            if not TEST:
+                print("bad URL")
+                conn.sendall(b'HTTP/1.1 400 Bad Request\n')
             else:
-                NAME = socket.gethostbyaddr(GET)
-                BUFFER = 'HTTP/1.1 200 OK\n' + GET + ":PTR=" + NAME[0] + "\n"
-                conn.sendall(BUFFER.encode())
+                GET = re.findall('name=.*&', BUFFER)
+                if GET:
+                    GET = GET[0][5:-1]
+                    REQUEST = re.findall('type=A HTTP/1.1', BUFFER)
+                    #A hostname -> IP
+                    if REQUEST:
+                        ADDR = socket.gethostbyname(GET)
+                        BUFFER = 'HTTP/1.1 200 OK\n' + GET + ":A=" + ADDR + "\n"
+                        conn.sendall(BUFFER.encode())
+                    #PTR IP -> hostname
+                    else:
+                        REQUEST = re.findall('type=PTR HTTP/1.1', BUFFER)
+                        if REQUEST:
+                            NAME = socket.gethostbyaddr(GET)
+                            BUFFER = 'HTTP/1.1 200 OK\n' + GET + ":PTR=" + NAME[0] + "\n"
+                            conn.sendall(BUFFER.encode())
+                        else:
+                            print("type not A or PTR")
+                            conn.sendall(b'HTTP/1.1 400 Bad Request\n')
+                else:
+                    print("bad arg")
+                    conn.sendall(b'HTTP/1.1 400 Bad Request\n')
+        #POST or UNKNOWN
         else:
             REQUEST = re.search("POST", BUFFER)
             if REQUEST:
-                print("BUFFER:",BUFFER)
-                #get all requests
-                BUFFER = re.findall('.*:(?:A|PTR)$', BUFFER, flags=re.MULTILINE)
-                HOSTNAMES = ""
-                IPADDR = ""
-                for i in BUFFER:
-                    mezikod = re.search('.*:PTR$', i, flags=re.MULTILINE)
-                    mezikod2 = re.search('.*:A$', i, flags=re.MULTILINE)
-                    if mezikod:
-                        mezikod = mezikod.string
-                        HOSTNAMES = HOSTNAMES + "\n" + mezikod
-                    if mezikod2:
-                        mezikod2 = mezikod2.string
-                        IPADDR = IPADDR + "\n" + mezikod2
-                    print("HOSTNAMES:", HOSTNAMES)
-                    print("IP:", IPADDR)
-                print("BUFFER:",BUFFER)
-                print("POST")
+                TEST = re.search("/dns-query", BUFFER)
+                if not TEST:
+                    print("bad URL")
+                    conn.sendall(b'HTTP/1.1 400 Bad Request\n')
+                else:
+                    #get all requests
+                    BUFFER = re.findall('.*:(?:A|PTR)$', BUFFER, flags=re.MULTILINE)
+                    if BUFFER:
+                        Output = ""
+                        for i in BUFFER:
+                            mezikod = re.search('.*:PTR$', i, flags=re.MULTILINE)
+                            mezikod2 = re.search('.*:A$', i, flags=re.MULTILINE)
+                            if mezikod:
+                                mezikod = mezikod.string
+                                something = mezikod[:-4]
+                                neco = socket.gethostbyaddr(something)
+                                if not neco:
+                                    print("error")
+                                    conn.sendall(b'HTTP/1.1 500 Internal Server Error\n')
+                                Output += mezikod + "=" + neco[0] + "\n"
+                            if mezikod2:
+                                mezikod2 = mezikod2.string
+                                something = mezikod2[:-2]
+                                neco = socket.gethostbyname(something)
+                                if not neco:
+                                    print("error")
+                                    conn.sendall(b'HTTP/1.1 500 Internal Server Error\n')
+                                Output += mezikod2 + "=" + neco + "\n"
+                        print(Output)
+                        BUFFER = 'HTTP/1.1 200 OK\n' + Output
+                        conn.sendall(BUFFER.encode())
+                    else:
+                        print("bad post arg")
+                        conn.sendall(b'HTTP/1.1 400 Bad Request\n')
+            #not GET or POST
             else:
-                print("NOTHING")
+                print("unknown method")
+                conn.send(b'HTTP/1.1 405 Method Not Allowed\n')
     conn.close()
--- IPK/client.sh
+++ IPK/client.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-#curl -i localhost:$1/resolve?name=147.229.14.131\&type=PTR
-curl --data-binary @queries.txt -X POST http://localhost:$1/dns-query
+echo "bad GET"
+curl -i localhost:$1/resolv?name=147.229.14.131\&type=PTR
+echo "bad POST"
+curl -i --data-binary @queries.txt -X POST http://localhost:$1/dns-quer
+echo "good GET"
+curl -i localhost:$1/resolve?name=147.229.14.131\&type=PTR
+echo "good POST"
+curl -i --data-binary @queries.txt -X POST http://localhost:$1/dns-query
