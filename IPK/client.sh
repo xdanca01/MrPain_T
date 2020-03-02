dc69ab236330a7791be89d327ffd4ce369f6f81b
xdanca01 <petrik2008@email.cz>
2020-03-02 21:37:10 +0100

    ERROR 404

--- IPK/client.sh
+++ IPK/client.sh
@@ -4,6 +4,6 @@ curl -i localhost:$1/resolve?name=147.229.14.131&type=PTR
 echo "neco"
 curl -i --data-binary @queries.txt -X POST http://localhost:$1/dns-quer
 echo "neco"
-curl  localhost:$1/resolve?name=www.fit.vutbr.cz\&type=A
+curl  localhost:$1/resolve?name=www.fyt.vutbr.cz\&type=A
 curl  localhost:$1/resolve?name=147.229.14.131\&type=PTR
 curl  --data-binary @queries.txt -X POST http://localhost:$1/dns-query
