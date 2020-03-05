b1ca296fd32b7a30081770043a702ac95b2a37df
xdanca01 <petrik2008@email.cz>
2020-03-05 13:39:46 +0100

    smthg

--- IPP/test.php
+++ IPP/test.php
@@ -4,9 +4,14 @@
 $longoptions = array("directory::","parse-script::","int-script::","jexamxml::","help","recursive",
 "parse-only","int-only");
 $options = getopt("",$longoptions);
+$dirpath = "";
+$recursive = FALSE;
+$parse_script = "parse.php";
+$int_script = "interpret.py";
+$parse_only = FALSE;
+$int_only = FALSE;
+$jexamxml = "/pub/courses/ipp/jexamxml/jexamxml.jar";
 
-for ($i = 0;$i < $argc;++$i)
-{
     if(array_key_exists("help",$options) == TRUE)
     {
         if($argc != 2) exit(10);
@@ -15,6 +20,24 @@ for ($i = 0;$i < $argc;++$i)
         echo "written by Petr Dancak\n";
         exit(0);
     }
-}
+    if(array_key_exists("directory",$options) == TRUE)
+    {
+        $dirpath = $options["directory"];
+    }
+    if(array_key_exists("recursive",$options) == TRUE) $recursive = TRUE;
+    if(array_key_exists("parse-script",$options) == TRUE) $parse_script = $options["parse-script"];
+    if(array_key_exists("int-script",$options) == TRUE) $int_script = $options["int-script"];
+    if(array_key_exists("parse-only",$options) == TRUE)
+    {
+        if(strcmp($int_script,"interpret.py"))exit(10);
+        $parse_only = TRUE;
+    }
+    if(array_key_exists("int-only",$options) == TRUE)
+    {
+        if(strcmp($parse_script,"parse.php") || $parse_only == TRUE)exit(10);
+        $int_only = TRUE;
+    }
+    if(array_key_exists("jexamxml",$options) == TRUE) $jexamxml = $options["jexamxml"];
+
 exit(0);
 ?>
