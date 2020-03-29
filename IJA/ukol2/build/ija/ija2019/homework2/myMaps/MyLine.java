bd6c0d1d68b688a734fe1ca35bd939be4e9910de
xdanca01 <pdancak@redhat.com>
2020-03-29 20:39:16 +0200

    m

---
+++ IJA/ukol2/build/ija/ija2019/homework2/myMaps/MyLine.java
@@ -0,0 +1,82 @@
+package ija.ija2019.homework2.myMaps;
+
+import ija.ija2019.homework2.maps.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.AbstractMap.SimpleEntry;
+
+public class MyLine implements Line{
+
+    private String id;
+    private ArrayList<Street> ulice;
+    private ArrayList<Stop> zastavky;
+
+
+    public MyLine(String s)
+    {
+        this.id = s;
+        this.ulice = new ArrayList<Street>();
+        this.zastavky = new ArrayList<Stop>();
+    }
+
+
+
+    public boolean addStop(Stop stop)
+    {
+        Street s = stop.getStreet();
+        int delka = this.zastavky.size();
+        if(delka == 0)
+        {
+            this.zastavky.add(stop);
+            this.ulice.add(s);
+            return true;
+        }
+        return false;
+        
+
+    }
+
+
+    public List<SimpleEntry<Street,Stop>> getRoute()
+    {
+        
+        List l = new ArrayList<SimpleEntry<Street,Stop>>();
+        int delka = this.ulice.size();
+        int cnt = 0;
+        for(int i = 0; i < delka; ++i)
+        {
+            Street s1 = this.ulice.get(i);
+            Stop stop = this.zastavky.get(cnt);
+            Street s2 = stop.getStreet();
+            SimpleEntry Se;
+            if(s1.getId() == s2.getId())
+            {
+                Se = new SimpleEntry<Street,Stop>(s1,stop);
+                ++cnt;
+            }
+            else Se = new SimpleEntry<Street,Stop>(s1, null);
+            l.add(Se);
+        }
+        return l;
+
+    }
+
+    /**
+     * Přidá ulici do mapy.
+     * @param s Objekt reprezentující ulici.
+     */
+    public boolean addStreet(Street s)
+    {
+        if(this.ulice.size() == 0)
+        {
+            if(s.getStops().size() == 0) return false;
+            this.ulice.add(s);
+            return true;
+        }
+        Street vzt = this.ulice.get(this.ulice.size() - 1);
+        if(vzt.end() != s.begin() && vzt.end() != s.end() && vzt.begin() != s.begin() && vzt.begin() != s.end()) return false;
+        this.ulice.add(s);
+        return true;
+    }
+
+}
---
+++ IJA/ukol2/build/ija/ija2019/homework2/myMaps/MyStreet.java
@@ -0,0 +1,135 @@
+package ija.ija2019.homework2.myMaps;
+import ija.ija2019.homework2.maps.*;
+import java.util.ArrayList;
+
+
+public class MyStreet implements Street
+{
+    private String Id;
+    private ArrayList<Coordinate> seznam;
+    private ArrayList<Stop> zastavky;
+
+    public MyStreet(String jmeno, Coordinate souradnice1, Coordinate souradnice2)
+    {
+        this.Id = jmeno;
+        this.seznam = new ArrayList<Coordinate>();
+        this.zastavky = new ArrayList<Stop>();
+        this.seznam.add(souradnice1);
+        this.seznam.add(souradnice2);
+    }
+
+    public MyStreet(String jmeno, Coordinate souradnice1, Coordinate souradnice2, Coordinate c3)
+    {
+        this.Id = jmeno;
+        this.seznam = new ArrayList<Coordinate>();
+        this.zastavky = new ArrayList<Stop>();
+        this.seznam.add(souradnice1);
+        this.seznam.add(souradnice2);
+        this.seznam.add(c3);
+    }
+
+
+
+    public MyStreet()
+    {
+        this.Id = null;
+        this.seznam = new ArrayList<Coordinate>();
+        this.zastavky = new ArrayList<Stop>();
+    }
+
+    /**
+     * Vrátí identifikátor ulice.
+     * @return Identifikátor ulice.
+     */
+    public String getId()
+    {
+        return this.Id;
+    }
+
+    /**
+     * Vrátí seznam souřadnic definujících ulici. První v seznamu je vždy počátek a poslední v seznamu konec ulice.
+     * @return Seznam souřadnic ulice.
+     */
+    public ArrayList<Coordinate> getCoordinates()
+    {
+        return this.seznam;
+    }
+
+    //Vrátí souřadnice začátku ulice
+    public Coordinate begin()
+    {
+        return this.seznam.get(0);
+    }
+
+    //Vrátí souřadnice konce ulice
+    public Coordinate end()
+    {
+        return this.seznam.get(seznam.size() - 1);
+    }
+
+    /**
+     * Vrátí seznam zastávek na ulici.
+     * @return Seznam zastávek na ulici. Pokud ulice nemá žádnou zastávku, je seznam prázdný.
+     */
+    public ArrayList<Stop> getStops()
+    {
+        return this.zastavky;
+    }
+
+    /**
+     * Přidá do seznamu zastávek novou zastávku.
+     * @param stop Nově přidávaná zastávka.
+     */
+    public boolean addStop(Stop stop)
+    {   
+        int delka = this.seznam.size();
+        Coordinate c3 = stop.getCoordinate();
+        for(int i = 1; i < delka; ++i)
+        {
+            Coordinate c1 = this.seznam.get(i - 1);
+            Coordinate c2 = this.seznam.get(i);
+            int diffx = c1.getX() - c2.getX();
+            int diffy = c1.getY() - c2.getY();
+            if(diffx == 0)
+            {
+                if(c3.getX() == c1.getX() && ((c3.getY() >= c1.getY() && c3.getY() <= c2.getY()) || (c3.getY() <= c1.getY() && c3.getY() >= c2.getY()) ))
+                {
+                    stop.setStreet(this);
+                    this.zastavky.add(stop);
+                    return true;
+                }
+            }
+            else if(diffy == 0)
+            {
+                if(c3.getY() == c1.getY() && ((c3.getX() >= c1.getX() && c3.getX() <= c2.getX()) || (c3.getX() <= c1.getX() && c3.getX() >= c2.getX()) ))
+                {
+                    this.zastavky.add(stop);
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+
+    public boolean follows(Street s)
+    {
+        if(this.end().equals(s.begin()) || this.begin().equals(s.end()) || this.end().equals(s.end()) || this.begin().equals(s.begin())) return true;
+        return false;
+    }
+
+    public boolean equals(Object obj)
+    {
+        if (obj == this) return true;
+
+        else if (obj == null || obj.getClass() != this.getClass()) return false;
+
+        Street test = (Street) obj;
+        return test.getId() == this.getId();
+
+    }
+
+
+
+}
